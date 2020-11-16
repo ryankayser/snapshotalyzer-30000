79236fcae57a2977c5f53605f6550a27ec71c859
2163963 <2163963@wholefoods.com>
2020-11-15 20:15:16 -0600

    Fixed printf typo

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -97,7 +97,7 @@ def create_snapshots(project):
         i.wait_until_stopped()
         for v in i.volumes.all():
             if has_pending_snapshot(v):
-                printf("  Skipping {0}, snapshot already in progress.".format(v.id))
+                print("  Skipping {0}, snapshot already in progress.".format(v.id))
                 continue
             print("  Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by SnapshotAlyzer 30000")
